967d759ce1b9d2c10dbc73f177a8fd8ef01965eb
lcl <livdipe@gmail.com>
2016-02-19 19:08:57 +0800

    editor script初探，HelloWorld.cs文件可以放在不同的目录

---
+++ Assets/Editor/HelloWorld.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class HelloWorld 
+{
+	[MenuItem ("GameObject/Create HelloWorld")]
+	private static void CreateHelloWorldGameObject ()
+	{
+		if (EditorUtility.DisplayDialog(
+			"Hello World",
+			"Do you really want to do this?",
+			"Create",
+			"Cancel"))
+		{
+			new GameObject("HelloWorld");
+		}
+	}
+}
